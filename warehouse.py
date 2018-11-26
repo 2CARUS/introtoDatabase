7a516dd860eeebd3e5b55cf3baa42f2e7d57842c
2CARUS <c.kopp.professional@gmail.com>
2018-11-26 13:25:16 -0600

    adding some comments and class definitions for making pages and main view of program

--- warehouse.py
+++ warehouse.py
@@ -1,24 +1,72 @@
 import tkinter as tk
 from mysql.connector import (connection)
 
-# Function Definitions:
+# make a page
+class Page(tk.Frame):
+    # Frame is a rectangular area of a window
+    # inherit page from Frame as a simple superclass to other Pages
+    # it's only method is show(), which simply pulls it to the top
+    #   of the viewing stack
 
-# link buttons to moving screens
-
-# link form submisison to SQL database
-
-
-# main:
-
-
-# create baseline windows
-root = tk.Tk()
-
-
-# create window navigation buttons
-
-
-# create FORM1
+    def __init__(self, master=None, cnf={}, **kw):
+        super().__init__(master=master, cnf=cnf, **kw)
+        #auto-generated initializer method
+        # note: all tkinter objects need a master Frame/Window/Whatever
+        # with which to latch onto. see ROOT
+    
+    def show(self):
+        self.lift()
 
 
-#
+
+# Simple record pull
+class Page1(Page):
+    def __init__(self, master=None, cnf={}, **kw):
+        super().__init__(master=master, cnf=cnf, **kw)
+
+    pass
+
+
+
+# Main view window (initializes everything else)
+
+class MainView(tk.Frame):
+    def __init__(self, master=None, cnf={}, **kw):
+        super().__init__(master=master, cnf=cnf, **kw)
+        '''
+        This mainview class may or may not be
+        an actual main view. Instead of calling
+        self.title(), it tries to pull from the toplevel
+        structure (which could be itself) and change
+        that title
+        '''
+        self.winfo_toplevel().title("Toolhouse Search")
+
+        # initialize pages
+        #   page1 = page(self)
+        #   ...
+
+        # initialize naviagation frames
+        buttonframe = tk.Frame(self)    # to hold buttons
+        containter = tk.Frame(self)     # to hold form
+        buttonframe.grid(row=0, column=0, sticky = 'W')
+        
+
+### Initialize 
+
+'''
+This file could be imported somewhere else later
+A python convention is to check if it's the main,
+then execute what should be main-type code stuff
+'''
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    # ROOT
+    # proper tkinter programs have a root which the other
+    #   frames(pieces of window) and widgets (things in frames)
+    #   are rooted into
+    main = MainView(root)
+    main.pack(side="top", fill="both", expand=True)
+    root.wm_geometry("400x400")
+    root.mainloop()
