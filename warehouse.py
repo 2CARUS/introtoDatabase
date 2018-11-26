8dcee2a726d403dd4a0d45981cb758a60e1c24bc
2CARUS <c.kopp.professional@gmail.com>
2018-11-26 11:22:02 -0600

    setting up some initial tasks

--- warehouse.py
+++ warehouse.py
@@ -1,3 +1,24 @@
-from tkinter import *
+import tkinter
+from mysql.connector import (connection)
 
-root = Tk()
+# Function Definitions:
+
+# link buttons to moving screens
+
+# link form submisison to SQL database
+
+
+# main:
+
+
+# create baseline window
+root = tkinter.Tk()
+
+
+# create window navigation buttons
+
+
+# create FORM1
+
+
+#
